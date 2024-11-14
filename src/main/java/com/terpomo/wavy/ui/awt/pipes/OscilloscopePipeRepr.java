77a64d7c8b2589947cd0453a14c5564997c05061
Rodolfo Souza <rodolfoboffo@gmail.com>
2024-11-14 10:21:28 -0300

    IMprovements on oscilloscope

--- src/main/java/com/terpomo/wavy/ui/awt/pipes/OscilloscopePipeRepr.java
+++ src/main/java/com/terpomo/wavy/ui/awt/pipes/OscilloscopePipeRepr.java
@@ -1,22 +1,28 @@
 package com.terpomo.wavy.ui.awt.pipes;
 
-import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.LayoutManager;
+import java.awt.*;
+import java.io.Serial;
 
 import javax.swing.JPanel;
 
 import com.terpomo.wavy.flow.IPort;
 import com.terpomo.wavy.pipes.OscilloscopePipe;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultValueDataset;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class OscilloscopePipeRepr extends AbstractPipeRepr {
 
+	@Serial
 	private static final long serialVersionUID = 7368297233394330359L;
-	private LayoutManager contentLayout;
-	private JPanel portsPanel;
-	private JPanel chartPanel;
-	
+	private final JFreeChart lineChart;
+	private final LayoutManager contentLayout;
+	private final JPanel portsPanel;
+	private final ChartPanel chartPanel;
+	private final XYSeriesCollection dataset;
+
 	public OscilloscopePipeRepr(OscilloscopePipe pipe, String name) {
 		super(pipe, name);
 		JPanel panel = this.getContentPanel();
@@ -31,10 +37,13 @@ public class OscilloscopePipeRepr extends AbstractPipeRepr {
 		
 		this.createPipePropertiesForInputs(pipe);
 		this.layoutPipePropertiesOnGrid(this.portsPanel);
-		
-		this.chartPanel = new JPanel();
+
+		this.dataset = new XYSeriesCollection();
+		this.lineChart = ChartFactory.createXYLineChart(null, null, null, this.dataset);
+		this.chartPanel = new ChartPanel(this.lineChart);
 		this.chartPanel.setLayout(new BorderLayout());
-//		this.chartPanel.add(BorderLayout.CENTER, new jfreechar.)
+		this.chartPanel.setPreferredSize(new Dimension(400, 200));
+
 		constraints = new GridBagConstraints();
 		constraints.gridx = 0;
 		constraints.gridy = 1;
