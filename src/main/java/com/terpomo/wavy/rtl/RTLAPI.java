525dbd96cde7a999ac617383e5637a6b3855e384
Rodolfo Souza <rodolfoboffo@gmail.com>
2025-04-15 23:28:04 -0300

    Calling the first functions from RTL

--- src/main/java/com/terpomo/wavy/rtl/RTLAPI.java
+++ src/main/java/com/terpomo/wavy/rtl/RTLAPI.java
@@ -2,7 +2,7 @@ package com.terpomo.wavy.rtl;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
-import com.sun.jna.Structure;
+import com.sun.jna.Pointer;
 
 public interface RTLAPI extends Library {
 
@@ -10,6 +10,6 @@ public interface RTLAPI extends Library {
 
     int rtlsdr_get_device_count();
     String rtlsdr_get_device_name(int index);
-    int rtlsdr_open(RTLSDRDevice device, int index);
-
+    int rtlsdr_open(Pointer[] p, int index);
+    int rtlsdr_get_tuner_gains(Pointer p, int[] gains);
 }
--- src/main/java/com/terpomo/wavy/rtl/RTLTest.java
+++ src/main/java/com/terpomo/wavy/rtl/RTLTest.java
@@ -1,14 +1,23 @@
 package com.terpomo.wavy.rtl;
 
+import com.sun.jna.Pointer;
+
+import java.util.Arrays;
+
 public class RTLTest {
 
     public static void main(String[] args) {
         int count = RTLAPI.INSTANCE.rtlsdr_get_device_count();
         for (int i = 0; i < count; i++) {
             System.out.println(RTLAPI.INSTANCE.rtlsdr_get_device_name(i));
-            RTLSDRDevice device = new RTLSDRDevice();
-            int r = RTLAPI.INSTANCE.rtlsdr_open(device, i);
-            System.out.println(String.format("open result: %d", r));
+            Pointer[] devicePtr = new Pointer[1];
+            int result = RTLAPI.INSTANCE.rtlsdr_open(devicePtr, i);
+            System.out.println(String.format("Open result: %d", result));
+            if (result >= 0) {
+                int[] gains = new int[100];
+                int nGains = RTLAPI.INSTANCE.rtlsdr_get_tuner_gains(devicePtr[0], gains);
+                System.out.println(String.format("Number of gains: %d, %s", nGains, Arrays.toString(Arrays.stream(gains).filter(g->g!=0).toArray())));
+            }
             break;
         }
     }
