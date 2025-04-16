edce970c0bc2a3aba5a42768cc17286aeab8a7fd
Rodolfo Souza <rodolfoboffo@gmail.com>
2025-04-16 20:59:27 -0300

    Adding more functions to RTL API

--- src/main/java/com/terpomo/wavy/rtl/RTLAPI.java
+++ src/main/java/com/terpomo/wavy/rtl/RTLAPI.java
@@ -11,5 +11,21 @@ public interface RTLAPI extends Library {
     int rtlsdr_get_device_count();
     String rtlsdr_get_device_name(int index);
     int rtlsdr_open(Pointer[] p, int index);
+    int rtlsdr_close(Pointer p);
+    int rtlsdr_get_sample_rate(Pointer p);
+    int rtlsdr_set_sample_rate(Pointer p, int sampleRate);
     int rtlsdr_get_tuner_gains(Pointer p, int[] gains);
+    int rtlsdr_get_tuner_gain(Pointer p);
+    int rtlsdr_set_tuner_gain(Pointer p, int gain);
+    int rtlsdr_get_center_freq(Pointer p);
+    long rtlsdr_get_center_freq64(Pointer p);
+    int rtlsdr_set_center_freq(Pointer p, int freq);
+    int rtlsdr_set_center_freq64(Pointer p, long freq);
+    int rtlsdr_get_direct_sampling(Pointer p);
+    int rtlsdr_set_direct_sampling(Pointer p, boolean on);
+    int rtlsdr_read_sync(Pointer p, byte[] buffer, int bufferSize, int[] bytesRead);
+    int rtlsdr_reset_buffer(Pointer p);
+    int rtlsdr_set_testmode(Pointer p, boolean on);
+    int rtlsdr_set_ds_mode(Pointer p, int mode, int freqThreshold);
+
 }
--- src/main/java/com/terpomo/wavy/rtl/RTLTest.java
+++ src/main/java/com/terpomo/wavy/rtl/RTLTest.java
@@ -8,15 +8,49 @@ public class RTLTest {
 
     public static void main(String[] args) {
         int count = RTLAPI.INSTANCE.rtlsdr_get_device_count();
-        for (int i = 0; i < count; i++) {
-            System.out.println(RTLAPI.INSTANCE.rtlsdr_get_device_name(i));
+        for (int deviceIndex = 0; deviceIndex < count; deviceIndex++) {
+            System.out.println(RTLAPI.INSTANCE.rtlsdr_get_device_name(deviceIndex));
             Pointer[] devicePtr = new Pointer[1];
-            int result = RTLAPI.INSTANCE.rtlsdr_open(devicePtr, i);
+            int result = RTLAPI.INSTANCE.rtlsdr_open(devicePtr, deviceIndex);
             System.out.println(String.format("Open result: %d", result));
             if (result >= 0) {
+                result = RTLAPI.INSTANCE.rtlsdr_reset_buffer(devicePtr[0]);
+                System.out.println(String.format("Reset buffer result: %d", result));
+                result = RTLAPI.INSTANCE.rtlsdr_set_testmode(devicePtr[0], false);
+                System.out.println(String.format("Enable test mode result: %d", result));
+                result = RTLAPI.INSTANCE.rtlsdr_set_ds_mode(devicePtr[0], RTLDataSamplingMode.RTLSDR_DS_IQ.getValue(), 28000000);
+                System.out.println(String.format("Set DS Mode: %d", result));
                 int[] gains = new int[100];
                 int nGains = RTLAPI.INSTANCE.rtlsdr_get_tuner_gains(devicePtr[0], gains);
-                System.out.println(String.format("Number of gains: %d, %s", nGains, Arrays.toString(Arrays.stream(gains).filter(g->g!=0).toArray())));
+                gains = Arrays.stream(gains).filter(g->g!=0).toArray();
+                System.out.println(String.format("Number of gains: %d, %s", nGains, Arrays.toString(gains)));
+                result = RTLAPI.INSTANCE.rtlsdr_set_tuner_gain(devicePtr[0], gains[gains.length-1]);
+                System.out.println(String.format("Set gain result: %d", result));
+                int tunerGain = RTLAPI.INSTANCE.rtlsdr_get_tuner_gain(devicePtr[0]);
+                System.out.println(String.format("Tuner gain: %d", tunerGain));
+                result = RTLAPI.INSTANCE.rtlsdr_set_sample_rate(devicePtr[0], 2048000);
+                System.out.println(String.format("Set sample rate result: %d", result));
+                int sampleRate = RTLAPI.INSTANCE.rtlsdr_get_sample_rate(devicePtr[0]);
+                System.out.println(String.format("Sample Rate: %d", sampleRate));
+                result = RTLAPI.INSTANCE.rtlsdr_set_center_freq(devicePtr[0], 106300000);
+                System.out.println(String.format("Set frequency result: %d", result));
+                long centerFreq = RTLAPI.INSTANCE.rtlsdr_get_center_freq64(devicePtr[0]);
+                System.out.println(String.format("Center Frequency: %d", centerFreq));
+                result = RTLAPI.INSTANCE.rtlsdr_set_direct_sampling(devicePtr[0], false);
+                System.out.println(String.format("Set Direct Sample result: %d", result));
+                int dsMode = RTLAPI.INSTANCE.rtlsdr_get_direct_sampling(devicePtr[0]);
+                System.out.println(String.format("Direct sample mode: %d", dsMode));
+                int BUFFER_SIZE = 262144;
+                byte[] buffer = new byte[BUFFER_SIZE];
+                result = 0;
+                for (int i = 0; i < 13; i++) {
+                    int[] readResult = new int[1];
+                    result = RTLAPI.INSTANCE.rtlsdr_read_sync(devicePtr[0], buffer, BUFFER_SIZE, readResult);
+                    System.out.println(String.format("Read result: %d", result));
+                    if (result < 0) break;
+                }
+                result = RTLAPI.INSTANCE.rtlsdr_close(devicePtr[0]);
+                System.out.println(String.format("Close result: %d", result));
             }
             break;
         }
